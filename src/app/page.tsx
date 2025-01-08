905492c2c3c43d913a3cfab8256799609bfaa35a
Anneme <Anneme.Holzh@gmail.com>
2025-01-08 20:45:38 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Hero from "./_components/Hero";
 import { Suspense } from "react";
 import { serverClient } from "./_trpc/serverClient";
-import TodoListWrapper from "./_components/TodoListWrapper";
 import TodoList from "./_components/TodoList";
 
 export default async function Home() {
   const todos = await serverClient.getTodos.query();
+
   return (
     <main className="flex flex-col items-center justify-start">
       <section className="hero-section w-full h-screen flex items-center justify-center">
@@ -13,7 +13,7 @@ export default async function Home() {
       </section>
       <section className="w-full h-screen flex items-center justify-center">
         <Suspense fallback={<div>Loading...</div>}>
-          <TodoList />
+          <TodoList initialTodos={todos} />
         </Suspense>
       </section>
     </main>
